a0a64aa1098011fcf61a45b2314769afe4919304
Harshit Chakraborty <harshitchakraborty49@gmail.com>
2026-02-12 20:15:11 +0530

    Add math functions for user input in math.py

---
+++ math.py
@@ -0,0 +1,8 @@
+#importing function
+import math as ma
+#user input
+a= int(input("Enter a number: "))
+#performing math func
+print("Square root: ", ma.sqrt(a))
+print("Logarithm: ", ma.log(a))
+print("Sine: ", ma.sin(a))
